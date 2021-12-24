26cd64157717b7b23efe89171518f582c8f6803c
Andrey Deriabin <aderiabin.98@gmail.com>
2021-12-24 17:23:37 +0300

    Update database.py

--- database.py
+++ database.py
@@ -11,8 +11,13 @@ def get_random():
 def add_like(id: int):
     with open('memes.json', 'r') as file:
         db = json.load(file)
-    db['entries'][id]['likes'] += 1
-    like_count = db['entries'][id]['likes']
+    for i in range(len(db['entries'])):
+        if db['entries'][i]['id'] == id:
+            db['entries'][i]['likes'] += 1
+            like_count = db['entries'][i]['likes']
+            break
+    else: # выполнится, если цикл for не был прерван через break
+        return 'no such id'
     with open('memes.json', 'w') as file:
         json.dump(db, file)
     return like_count
